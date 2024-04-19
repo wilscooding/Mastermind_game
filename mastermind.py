5c46a9ac83edc821fe709caec1aaf5c034885c2d
Wilkin Ruiz <wilkin.ruizvalerio@gmail.com>
2024-04-19 17:00:21 -0400

    checking guess hardcoded

--- mastermind.py
+++ mastermind.py
@@ -1,8 +1,7 @@
 import requests
 
+
 # Function to generate the secret code using RANDOM.ORG API
-
-
 def generate_secret_code():
     url = "https://www.random.org/integers/"
     params = {
@@ -28,4 +27,22 @@ def generate_secret_code():
             return None
 
 
-print(generate_secret_code())
+def check_guess(secret_code, guess):
+    correct_numbers_and_positions = 0
+    correct_numbers_only = 0
+
+    for i in range(len(secret_code)):
+        if guess[i] == secret_code[i]:
+            correct_numbers_and_positions += 1
+        elif guess[i] in secret_code:
+            correct_numbers_only += 1
+
+    return correct_numbers_and_positions, correct_numbers_only
+
+
+secret_code = generate_secret_code()
+print("Secret Code: ", secret_code)
+
+guess = [1, 3, 4, 6]
+result = check_guess(secret_code, guess)
+print("Result:", result)
